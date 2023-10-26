9278546a4b4710cdcbfd2e9bcbbc60b3762c838c
cesar3435ru <cesssuperbro3435@gmail.com>
2023-10-25 18:36:34 -0600

    Instalacion y configuracion de jwt

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -4,6 +4,10 @@ namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
+use Closure;
+use Illuminate\Auth\AuthenticationException;
+
+
 
 class Authenticate extends Middleware
 {
@@ -12,6 +16,21 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        return null;
+    }
+    //Con esta funcion no es necesario poner el token para que me devuelva la informacion del usuario
+    public function handle($request, Closure $next, ...$guards)
+    {
+        if ($jwt = $request->cookie('jwt')) {
+            $request->headers->set('Authorization', 'Bearer ' . $jwt);
+        }
+
+        try {
+            $this->authenticate($request, $guards);
+        } catch (AuthenticationException $e) {
+            return response(['message' => 'No authenticated'], 401);
+        }
+
+        return $next($request);
     }
 }
